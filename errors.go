e9a1d6e98bdb54bca9d779bdb903895e906436d3
0xsecret <0xsecret@gmail.com>
2016-02-24 21:58:41 +0900

    Create errors.go

---
+++ errors.go
@@ -0,0 +1,13 @@
+package statistics
+
+type statisticsError struct {
+  error string
+}
+
+func (e statisticsError) Error() string {
+  return e.error
+}
+
+var (                                           
+  EmptySlice = statisticsError{"slice is empty"}
+)
