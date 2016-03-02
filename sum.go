1dccd09fc7259390b3e07e3b805ddbcf12fdb2e5
0xsecret <0xsecret@gmail.com>
2016-03-02 15:03:55 +0900

    Update sum.go

--- sum.go
+++ sum.go
@@ -7,7 +7,7 @@ import (
 //return the sum of a sequence of a slice
 func Sum(slice []float64) (float64, error) {
   if len(slice) == 0 {
-    return math.NaN(), EmptySlice
+    return math.NaN(), EmptySliceError
   }
   var sum float64 = 0
   for i := 0; i < len(slice); i++ {
